c934ce8f5d15a83692c9adc803cc5138c6c03068
Ronny Hermans <rhermans@ad.ua.ac.be>
2021-11-18 11:48:02 +0100

    Added dbcontext

---
+++ Blzr/Server/Data/BlzrContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blzr.Shared;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace blzr1.Server.Data
+{
+    public class BlzrContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);
+          }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=blzr.db");
+        }
+    }
+}
