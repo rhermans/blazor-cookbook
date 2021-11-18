821c91a8fd391036a8767dfad954432f4e312b04
Ronny Hermans <rhermans@ad.ua.ac.be>
2021-11-18 11:45:43 +0100

    Added swagger

--- Blzr/Server/Program.cs
+++ Blzr/Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Blzr", Version = "v0.0.1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description =
+            "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+
+});
 
 var app = builder.Build();
 
@@ -25,6 +43,18 @@ app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
+
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+#if DEBUG
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blzr");
+#else
+                    c.SwaggerEndpoint("/mlserver/swagger/v1/swagger.json", "blzr");
+#endif
+    c.RoutePrefix = "swagger";
+});
 
 app.UseRouting();
 
