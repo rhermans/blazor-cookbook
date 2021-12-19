4a1f7412bd774d4e57ab7dfef2b7613cd72784f5
DESKTOP-L8EQJ1S\ronny <ronny.hermans@gmail.com>
2021-12-19 18:02:49 +0100

    Split UserState and UserFeature

---
+++ Blzr/Client/Store/UserStore/UserFeature.cs
@@ -0,0 +1,19 @@
+﻿using Blzr.Shared;
+using Fluxor;
+
+namespace Blzr.Client.Store.UserStore
+{
+    public class UserFeature : Feature<UserState>
+    {
+        public override string GetName() => nameof(UserState);
+
+
+        protected override UserState GetInitialState() =>
+            new UserState
+            {
+                Initialized = false,
+                Loading = false,
+                Users = Enumerable.Empty<User>()
+            };
+    }
+}
--- Blzr/Client/Store/UserStore/UserState.cs
+++ Blzr/Client/Store/UserStore/UserState.cs
@@ -13,17 +13,5 @@ namespace Blzr.Client.Store.UserStore
         public IEnumerable<User>? Users { get; init; }
     }
 
-    public class UserFeature : Feature<UserState>
-    {
-        public override string GetName() => nameof(UserState);
-
-
-        protected override UserState GetInitialState() =>
-            new UserState
-            {
-                Initialized = false,
-                Loading = false,
-                Users = Enumerable.Empty<User>()
-            };
-    }
+  
 }
