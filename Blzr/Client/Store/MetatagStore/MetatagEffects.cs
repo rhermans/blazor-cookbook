abe4146aada83d325930db0499791556c47a990c
Ronny Hermans <rhermans@ad.ua.ac.be>
2021-12-20 10:31:04 +0100

    Added effect to load metatags

--- Blzr/Client/Store/MetatagStore/MetatagEffects.cs
+++ Blzr/Client/Store/MetatagStore/MetatagEffects.cs
@@ -1,6 +1,25 @@
-﻿namespace Blzr.Client.Store.MetatagStore
+﻿using Fluxor;
+using Blzr.Shared;
+using System.Net.Http.Json;
+
+namespace Blzr.Client.Store.MetatagStore
 {
     public class MetatagEffects
     {
+        private readonly HttpClient _httpClient;
+
+        public MetatagEffects(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+
+        [EffectMethod(typeof(LoadMetatagsAction))]
+        public async Task LoadUsersActionAsync(IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch((new SetLoadingAction(true)));
+            var metatags = await _httpClient.GetFromJsonAsync<IEnumerable<Metatag>>("api/metatags");
+            dispatcher.Dispatch(new SetMetatagsLoadedAction(metatags));
+            dispatcher.Dispatch(new SetLoadingAction(false));
+        }
     }
 }
